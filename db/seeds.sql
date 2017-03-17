cd53e77bdec3089023324e012b6c321026c745c2
Derek M Phillips <derekmphillips@dereks-air.attlocal.net>
2017-03-16 22:03:03 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured, created_at) VALUES ('cheeseburger', false, '2019-04-14 20:31:43');
+INSERT INTO burgers (burger_name, devoured, created_at) VALUES ('turkeyburger', false, '2019-04-14 20:33:43');
